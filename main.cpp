904b51eb2ad8f1b92d859659b12162493911a501
loorenna <lorenaalisson2004@gmail.com>
2026-01-11 18:32:51 -0500

    correccion logica movida a mainwindow.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <iostream>
-#include <fstream>
-#include <string> 
-using namespace std;
 
-struct Usuario {
-    int id;
-    string nombre;
-    string cedula;
-    string correo;
-    string telefono;
-};
-
-void crearUsuario();
-void listarUsuarios();
-void modificarUsuario();
-void eliminarUsuario();
 
 int main(int argc, char *argv[])
 {
