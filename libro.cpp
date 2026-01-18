a28df0c50d34c4029ad8554e16d965a52691902f
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-01-17 19:12:47 -0500

    CRUD Libros

---
+++ libro.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "usuario.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QString>
+
+void guardarLibros(const libros &l) {
+
+    //Abre el archivo
+    QFile archivo("libros.txt");
+    if (!archivo.open(QIODevice::Append | QIODevice::Text)){
+        //QMessageBox sirve para mensajes de error con el uso del warning
+        //nullptr indica que el mensaje no esta asociado a alguna ventana
+        QMessageBox :: warning(nullptr, "Error" , "No se pudo abrir el archivo");
+        return;
+    }
+
+    //Crea un stream de salida, para poder escribir dentro del archivo usando el out
+    QTextStream out(&archivo);
+
+    //Escribe el texto 
+    out << l.idLibro << "|"
+        << l.titulo<< "|"
+        << l.autor << "|"
+        << l.anioPublicacion << "|"
+        << (l.disponibilidad ? "si" : "no" ) << "\n";
+   
+    //Cierra el archivo
+    archivo.close();
+}
--- libro.h
+++ libro.h
@@ -1,4 +1,16 @@
 #ifndef LIBRO_H
 #define LIBRO_H
+#include <string>
+using namespace std;
+
+struct libros{
+    int idLibro;
+    string titulo;
+    string autor;
+    int anioPublicacion;
+    bool disponibilidad;
+};
+
+void guardarLibros(const libros &l);
 
 #endif // LIBRO_H
