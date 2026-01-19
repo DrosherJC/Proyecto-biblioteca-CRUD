ac56a6febcd1eaadbc4e80b0e5026a25091e85c7
José Buñay <jose.bunay@epn.edu.ec>
2026-01-18 21:25:20 -0500

    Cabecera prestamos

--- prestamo.h
+++ prestamo.h
@@ -1,4 +1,16 @@
 #ifndef PRESTAMO_H
 #define PRESTAMO_H
 
+#include <QString>
+
+struct Prestamo
+{
+    int idPrestamo;
+    int idLibro;
+    int idUsuario;
+    QString fechaPrestamo;
+    QString fechaDevolucion;
+    bool devuelto;
+};
+
 #endif // PRESTAMO_H
